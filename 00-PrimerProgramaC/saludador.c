491d513e934338e62b18558913bcf91b7cb0d1f0
nachofulljs <ignaciom@fulljaus.com>
2022-03-30 14:05:39 -0300

    Primer commit.

---
+++ 00-PrimerProgramaC/saludador.c
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    system("clear");
+
+    cout << "Hola Desde C!!!"<< '\n';
+
+    return 0;
+}
