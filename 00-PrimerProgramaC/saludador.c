3c3fd9d6bc86bea1157a9dcd29af405abaa59274
nachoberaza <nachoberaza@gmail.com>
2022-03-30 14:55:11 -0300

    Corrigiendo temas de C.

--- 00-PrimerProgramaC/saludador.c
+++ 00-PrimerProgramaC/saludador.c
@@ -1,13 +1,9 @@
-#include <iostream>
-#include <cstdlib>
 #include <stdio.h>
 
-using namespace std;
-
 int main(){
-    system("clear");
+    //system("clear");
 
-    cout << "Hola Desde C!!!"<< '\n';
+    printf("Hola Desde C!!!\n");
 
     return 0;
 }
